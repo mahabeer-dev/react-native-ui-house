a75856448f9ff99e175d5a854d26a44be7a1d7e3
Gurpreet <sw.gurpreet012gmail.com>
2023-11-17 14:46:04 +0530

    fix(import): password input import issue fixed

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import {
   Card,
   Badge,
   CustomTextInput,
+  PasswordInput,
 } from "./src/components";
 
-export { Avatar, IconButton, Card, Badge, CustomTextInput };
+export { Avatar, IconButton, Card, Badge, CustomTextInput, PasswordInput };
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import { IconButton } from "./icon-nutton";
 import { Card } from "./card";
 import { Badge } from "./badge";
 import { CustomTextInput } from "./text-input";
+import { PasswordInput } from "./password-input";
 
-export { Avatar, IconButton, Card, Badge, CustomTextInput };
+export { Avatar, IconButton, Card, Badge, CustomTextInput, PasswordInput };
